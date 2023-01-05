dbae4029ec45786d14a0d6c5f5c047448ae38a2f
Tech-Kat <katpelle@pursuit.org>
2023-01-04 23:19:06 -0500

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Nav from "./Components/Nav";
+import Show from "./Pages/Show";
+import New from "./Pages/New";
+import Index from "./Pages/Index";
+import Home from "./Pages/Home";
+import Edit from "./Pages/Edit";
+import FourOFour from "./Pages/FourOFour";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/budget" element={<Index />} />
+            <Route path="/budget/new" element={<New />} />
+            <Route path="/budget/:index" element={<Show />} />
+            <Route path="/budget/:index/edit" element={<Edit />} />
+            <Route path="*" element={<FourOFour />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
